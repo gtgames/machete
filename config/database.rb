3d01167ef22a6df1c9e1fcd40540cbd6acd30944
Lorenzo <lorenzo@frenzart.com>
2010-09-28 17:57:30 +0200

    Production db bugfixes

--- config/database.rb
+++ config/database.rb
@@ -5,10 +5,9 @@ case Padrino.env
   when :development then DataMapper.setup( :default, "sqlite3://#{Padrino.root('db', 'development.db')}")
   when :test then DataMapper.setup( :default, "sqlite3://#{Padrino.root('db', 'test.db')}")
   when :production then DataMapper.setup(:default,
-    :adapter => 'postgresql',
-    :host => 'localhost',
+    :adapter => 'postgres',
+    :host => '127.0.0.1',
     :username => 'database_user',
     :password => 'database_password',
-    :database => 'database_name',
-    :encoding => 'utf8' )
+    :database => 'database_name')
 end
