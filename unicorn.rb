a435103ab70282e66c194ed57870a9616fcf705c
Lorenzo <lorenzo@frenzart.com>
2010-10-05 20:35:21 +0200

    new unicorns

--- unicorn.rb
+++ unicorn.rb
@@ -1,4 +1,3 @@
-ENV['GEM_PATH'] = "#{ENV['HOME']}/lib/ruby/gems:/opt/ruby/gems/"
 APP_ROOT = ::File::expand_path(File.dirname(__FILE__))
 
 env = 'production'
@@ -6,8 +5,8 @@ env = 'production'
 working_directory ::File::expand_path(File.dirname(__FILE__))
 
 worker_processes 2 # Master + 2 workers + app's background tasks
-pid APP_ROOT + "unicorn.pid"
-stderr_path APP_ROOT + "unicorn.log"
+pid APP_ROOT + "/unicorn.pid"
+stderr_path APP_ROOT + "/unicorn.log"
 
 #REE stuff
 if GC.respond_to?(:copy_on_write_friendly=)
