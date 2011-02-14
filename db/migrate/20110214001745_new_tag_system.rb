59ff9ba2c76a337c69ebbc8a7b6f0a5d36dca533
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-02-14 01:03:56 +0100

    new rael tag system

---
+++ db/migrate/20110214001745_new_tag_system.rb
@@ -0,0 +1,23 @@
+Sequel.migration do
+  up do
+    drop_table :post_taggins, :photo_taggins, :tags
+    
+    add_column :posts,  :tags, String
+    add_column :photos, :tags, String
+  end
+
+  down do
+    create_table :tags do
+      primary_key :id
+      String     :name, :null => false
+    end
+    create_table :post_taggins do
+      foreign_key :post_id, :posts
+      foreign_key :tag_id, :tags
+    end
+    create_table :photo_taggins do
+      foreign_key :photo_id, :photos
+      foreign_key :tag_id, :tags
+    end
+  end
+end
