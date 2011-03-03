aa51fc17eca3037bb6075d371cb0aa59fc59e597
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-03-03 18:39:06 +0100

    tag cloud in JS

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,8 +1,13 @@
 Frontend.controllers :tags do
-  get :index, :map => "/tags" do
+  get :index do
     redirect '/' if params[:t].nil?
     @posts = Post.tagged_with params[:t]
     @photos = Photo.tagged_with params[:t]
     render 'tags/list'
+  end
+
+  get :json do
+    content_type :json
+    return DB[:tags].all.to_json
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,18 @@ head.ready(function(){
       mto = $('mailto');
   mto.set('href', 'mailto:info@' + domain);
   mto.set('text', 'info@' + domain);
+
+  new Request.JSON({url: '/tags/json', onSuccess: function(tags){
+    var sum = 0;
+    tags.each(function(t){
+      sum += t.size;
+    });
+    tags.each(function(t){
+      new Element('a.tag', {
+        href: '/tags/?t=' + t.tag.replace(/\s+/, '+'),
+        style: "font-size:" + (t.size/(sum/tags.length))*100 + '%',
+        text: t.tag
+      }).inject($('tags'));
+    });
+  }}).get();
 });
