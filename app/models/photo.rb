9ddd4034cf539bd9003f2cfe1adf206619fdd57f
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-03-04 20:39:23 +0100

    added random photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,8 @@ class Photo < Sequel::Model
   
   def_dataset_method :full_text_search
   mount_uploader :file, ImageUploader
+  
+  def self.random(num = 1)
+    with_sql("SELECT * FROM \"#{table_name}\" ORDER BY RANDOM() LIMIT #{num}").all
+  end
 end
