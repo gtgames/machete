b759b88d57aa7dd240b2a39c85322f213bf280af
Lorenzo <lorenzo@frenzart.com>
2011-06-11 22:40:34 +0200

    better slug generator

--- public/javascripts/admin/application.js
+++ public/javascripts/admin/application.js
@@ -31,6 +31,20 @@
   $.fn.slugify = function(target){
     if (!this.length || !$(target).length) return -1;
 
+    function string_to_slug(str) {
+      str = str.replace(/^\s+|\s+$/g, ''); // trim
+      str = str.toLowerCase();
+      // remove accents, swap ñ for n, etc
+      var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+      var to   = "aaaaeeeeiiiioooouuuunc------";
+      for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+      }
+      str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+      return str;
+    }
     var that = this;
     this.userChanged = false;
 
@@ -38,7 +52,7 @@
       if (!that.userChanged) {
         $(target).attr(
           'value',
-          $(this).attr('value').toLowerCase().replace(/[^a-z0-9-]+/g, '-').replace(/[-]+/g, '-').replace(/^-|-$/g, '')
+          string_to_slug($(this).attr('value'))
         );
       }
     });
