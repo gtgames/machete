4f47ed25ab45c6859f44ca24c7c3ba052836eda2
Lorenzo <lorenzo@frenzart.com>
2011-06-24 10:34:47 +0200

    form builder

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,14 @@ class Machete < Padrino::Application
   register Padrino::Mailer
   register Padrino::Helpers
 
+  register PadrinoFields
+  set :default_builder, 'PadrinoFieldsBuilder'
+  PadrinoFields::Settings.configure do |config|
+    config.container = :div
+    config.label_required_marker = "<abbr>*</abbr>"
+    config.label_required_marker_position = :append
+  end
+
   register Frenz::AutoLocale
   set :locales, promise { Cfg[:locales] }
 
