2d5c8600fdec103243eb62ae90f72a747578fb35
Lorenzo <lorenzo@frenzart.com>
2011-06-24 02:10:07 +0200

    aggiunto /:lang/contact

---
+++ app/controllers/contact.rb
@@ -0,0 +1,15 @@
+Machete.controller :contact, :lang => I18n.language do
+  get :index do
+    @message = Message.new
+    render 'contact/index'
+  end
+
+  post :new do
+    @message = Message.new params[:message]
+    if @message.save
+      render 'contact/sent'
+    else
+      render 'contact/index'
+    end
+  end
+end
