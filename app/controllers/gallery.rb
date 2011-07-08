727da37ed714f45043b9dcd43c5fb27574000264
Lorenzo <lorenzo@frenzart.com>
2011-07-08 18:21:38 +0200

    pulizia route

--- app/controllers/gallery.rb
+++ app/controllers/gallery.rb
@@ -1,6 +1,6 @@
 Machete.controllers :gallery do
 
-  get :index, :map =>'/gallery' do
+  get :index do
     @galleries = Photo.galleries.map do |g|
       Photo.where(:gallery => g).skip(rand(Photo.count(:gallery => g))).limit(1).first
     end
