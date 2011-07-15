b225f5c0a2c048ae1b37694aa1cf47dc76b7e17c
Lorenzo <lorenzo@frenzart.com>
2011-07-15 20:47:35 +0200

    slug non poteva essere bianco

--- models/post.rb
+++ models/post.rb
@@ -13,6 +13,13 @@ class Post
 
   scope :by_slug, lambda { |slug| where(:slug => slug) }
 
-  validates_presence_of     :title, :slug, :lead, :text
-  validates_uniqueness_of   :title, :slug
+  validates_presence_of     :title, :lead, :text
+  validates_uniqueness_of   :title
+
+  before_save :slugify
+
+  private
+  def slugify
+    self.slug = self.title.to_slug
+  end
 end
