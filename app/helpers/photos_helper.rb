b2c559b29d65fd3a53c1a6dc5b43a74c0e57b1f9
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-09-06 14:22:23 +0200

    small changes to photos helper

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,6 +1,14 @@
 Machete.helpers do
   def photos_by_keywords(kwd, max=5)
-    Photo.where(:tags.in => kwd.to_s.split(',').map(&:downcase).uniq.map(&:strip)).limit(max)
+    photos = Photo.where(:tags.in => kwd.to_s.split(',').map(&:downcase).uniq.map(&:strip)).limit(max).all
+
+    if (count = photos.count) < max
+      ids = photos.collect(&:_id)
+      #db.photos.find({_id: {$nin: [ObjectId('4e65fa1efb28ef03540000c0'), ObjectId('4e65fa6efb28ef086200002d')] } })
+      Photo.where(:_id => {:$nin => ids } ).limit(max - count).each{|p| photos << p }
+    end
+
+    photos
   end
   def photos_by_gallery(gallery)
     Photo.where(:gallery_slug => gallery)
