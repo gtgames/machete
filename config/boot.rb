99775064c2612712b75f4cf44dae1c70e9c66926
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-09-24 15:47:52 +0200

    automatic symlink on boot
    TODO: think think think

--- config/boot.rb
+++ config/boot.rb
@@ -2,14 +2,31 @@
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unless defined?(PADRINO_ROOT)
 
-unless $LOAD_PATH.include? File.join(PADRINO_ROOT, 'lib_core')
-  $LOAD_PATH.unshift File.join(PADRINO_ROOT, 'lib_core') # this sucks ... but what could i do?
-end
-
 # Load Bundler
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, PADRINO_ENV)
+
+#
+# Machete Plugins
+#
+# TODO: Make something cleaner
+JSON.parse(File.new(Padrino.root('config', 'config.json'), 'r'))['plugins'].each do |plugin|
+  unless File.directory?( "#{PADRINO_ROOT}/module_#{plugin}" )
+    puts "Linking #{PADRINO_ROOT}/plugins/#{plugin}/module_#{plugin} => #{PADRINO_ROOT}/module_#{plugin}"
+    File.symlink "#{PADRINO_ROOT}/plugins/#{plugin}/module_#{plugin}", "#{PADRINO_ROOT}/module_#{plugin}"
+    
+    puts "Linking #{PADRINO_ROOT}/plugins/#{plugin}/templates/#{plugin} => #{PADRINO_ROOT}/templates/#{plugin}"
+    File.symlink "#{PADRINO_ROOT}/plugins/#{plugin}/templates/#{plugin}", "#{PADRINO_ROOT}/templates/#{plugin}"
+
+    if File.directory? "#{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin}"
+      puts "Linking #{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin} => #{PADRINO_ROOT}/templates/mailers/#{plugin}"
+      File.symlink "#{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin}", "#{PADRINO_ROOT}/templates/mailers/#{plugin}"
+    end
+  end
+end
+###
+
 ##
 # Add here your before load hooks
 #
@@ -20,7 +37,8 @@ Padrino.before_load do
 
   Wand.executable = '/usr/bin/file'
 
-  Padrino.cache = Padrino::Cache::Store::Mongo.new( MongoMapper.database, :size => 2, :max => 100, :collection => 'cache')
+  Padrino.cache = Padrino::Cache::Store::Mongo.new(
+    MongoMapper.database, :size => 2, :max => 100, :collection => 'cache' )
 
   -> {
     # compile less files in a closure :P
