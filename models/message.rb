4d57814f5a305c147a26445c42664eda3b4c05ac
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-09-26 20:07:27 +0200

    aggiunte richieste di contatto

--- models/message.rb
+++ models/message.rb
@@ -1,11 +1,17 @@
 class Message
   include MongoMapper::Document
   plugin Antispam
+  timestamps!
 
   key :author, String
   key :email, String
   key :phone, String
   key :text, String
+
+  # state
+  key :st, Integer, :default => 1 # 1  - unread state
+                                  # 0  - read
+                                  # -1 - deleted
 
   key :ip,  String, :default => lambda{ ENV['REMOTE_ADDR'] }
   key :ua,  String, :default => lambda{ ENV['HTTP_USER_AGENT'] }
