5d4294c10464fb6995a3bc210894cdcc372abb88
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-11-10 17:18:45 +0100

    helper for lazyloader script (TODO)

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -16,4 +16,22 @@ BasicApplication.helpers do
   def h text
     Rack::Utils.escape_html text
   end
+  
+  def lazy_img(thumb, options={}, href=nil)
+    o = {
+        w: '', h: '', title: ''
+    }
+    o.merge(options)
+    i  = '<img class="lazy" data-src="' << thumb << '" alt="' << o[:title] << '">'
+    ii = '<img src="' << thumb << '" alt="' << o[:title] << '">'
+    if href
+      '<a href="' << href << '">' << i << '</a>' << '<noscript><a href="' << href << '">' << ii << '</a></noscript>'
+    else
+      i << '<noscript>' << ii << '</noscript>'
+    end
+  end
+  
+  def lazy_thumb(file, w, h, title = '')
+    lazy_img file.thumb("#{w}x#{h}"), {w: w, h: h, title: title}, file.thumb(:big)
+  end
 end
