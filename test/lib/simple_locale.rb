b9b49bab29775742ce92697d699ccb83f84898eb
Lorenzo Giuliani <lorenzo@frenzart.com>
2011-11-21 12:07:45 +0100

    more tests in simple_locale

--- test/lib/simple_locale.rb
+++ test/lib/simple_locale.rb
@@ -5,16 +5,6 @@ require 'simple_locale'
 require 'riot'
 require "ap"
 
-app = lambda do |env|
-  [
-    200,
-    {
-      'Content-Type' => 'text/plain',
-      'Content-Length' => env['PATH_INFO'].length,
-    },
-    env['PATH_INFO']
-  ]
-end
 class Cfg
  def self.[](any)
    ['it', 'en']
@@ -29,35 +19,48 @@ class I18n
   end
 end
 
+def mock_get url
+  app = lambda do |env|
+    [
+      200,
+      { 'Content-Type' => 'text/plain'},
+      env['PATH_INFO']
+    ]
+  end
 
-context "redirects on " do
+  @request = Rack::MockRequest.env_for(url)
+
+  @response = Rack::AutoLocale.new(app, {
+    :host_blacklist => /^(www\.)?admin\..*$/,
+    :blacklist  => ['/media','/sitemap.xml', '/sitemap', '']
+  }).call(@request)
+  #@status, @headers, @body
+  @response
+end
+
+
+context "redirects on requests" do
   setup do
-    @request = Rack::MockRequest.env_for("/")
-
-    @response = Rack::AutoLocale.new(app, {
-      :host_blacklist => /^(www\.)?admin\..*$/,
-      :blacklist  => ['/media','/sitemap.xml', '/sitemap']
-    }).call(@request)
-    #@status, @headers, @body
-    @response
+    mock_get '/'
   end
   
+  asserts("it is not a 200") { topic[0] != 200 }
   asserts("it is a redirect") { topic[0] == 302 }
 end
 
-context "cleaup redirection path" do
+context "redirects existing url" do
   setup do
-    @request = Rack::MockRequest.env_for("/en/foobar")
-
-    @response = Rack::AutoLocale.new(app, {
-      :host_blacklist => /^(www\.)?admin\..*$/,
-      :blacklist  => ['/media','/sitemap.xml', '/sitemap']
-    }).call(@request)
-    #@status, @headers, @body
-    @response
+    mock_get '/en/foobar'
   end
   
   asserts("it is not redirect") { topic[0] != 302 }
-  asserts("body is equal to requested url minus language path") {
-    topic[2] == '/foobar' }
+  asserts("body is equal to requested url minus language path") { topic[2] == '/foobar' }
+end
+
+context "blacklists" do
+  setup do
+    mock_get '/media'
+  end
+  asserts("doesn't redirect on blacklist") { topic[0] == 200 }
+  asserts("PATH_INFO is not touched in blacklists") { topic[2] == '/media' }
 end
