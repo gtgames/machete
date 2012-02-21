d645594c0015fea8f66334af92bd4858430bc001
Lorenzo Giuliani <lorenzo@frenzart.com>
2012-02-21 20:10:41 +0100

    using a lib to manage plugins

--- config/apps.rb
+++ config/apps.rb
@@ -53,44 +53,10 @@ end
 
 #
 # Machete Plugins
-#
-# TODO: Make something cleaner
-JSON.parse(File.read(Padrino.root('config', 'config.json')))['plugins'].each do |plugin|
-  begin
-    unless File.directory?( "#{PADRINO_ROOT}/templates/#{plugin}" )
-      puts "Linking #{PADRINO_ROOT}/plugins/#{plugin}/templates/#{plugin} => #{PADRINO_ROOT}/templates/#{plugin}"
-      File.symlink "#{PADRINO_ROOT}/plugins/#{plugin}/templates/#{plugin}", "#{PADRINO_ROOT}/templates/#{plugin}"
-
-      if File.directory?("#{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin}") and not File.directory?("#{PADRINO_ROOT}/templates/mailer/#{plugin}")
-        puts "Linking #{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin} => #{PADRINO_ROOT}/templates/mailers/#{plugin}"
-        File.symlink "#{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin}", "#{PADRINO_ROOT}/templates/mailers/#{plugin}"
-      end
-    end
-  rescue Errno::EEXIST
-    # do nothing
-  end
-  Padrino.set_load_paths("#{PADRINO_ROOT}/plugins/#{plugin}", "#{PADRINO_ROOT}/plugins/#{plugin}/models")
-  require "#{PADRINO_ROOT}/plugins/#{plugin}/app"
-  Dir.glob("#{PADRINO_ROOT}/plugins/#{plugin}/models/*.rb").each{|r| require r.sub(/\.rb$/, '') }
-end
-###
-
-
-
+# lib/plugger.rb
+PluginManager(JSON.parse(File.read(Padrino.root('config', 'config.json')))['plugins'])
 
 Padrino.mount("Admin").to("/").host(/^(?:www\.)?admin\..*$/)
-
-Cfg['apps'].each do |app, mountpoint|
-  begin
-    Object.const_get(app) # testing app existance
-
-    puts "Mounting App: #{app}..."
-    Padrino.mount("#{app}").to("#{mountpoint.downcase}").host(/^(?!(admin|www\.admin)).*$/)
-  rescue NameError
-    # do nothing
-    logger.error "not mounting #{app}"
-  end
-end unless Cfg['apps'].nil?
 
 # This should have low priority
 Padrino.mount("Machete").to("/").host(/^(?!(admin|www\.admin)).*$/)
---
+++ lib/plugger.rb
@@ -0,0 +1,39 @@
+#
+# Machete Plugins
+#
+# TODO: Make something cleaner
+def PluginManager plugs
+  plugs.each do |plugin|
+    begin
+      unless File.directory?( "#{PADRINO_ROOT}/templates/#{plugin}" )
+        puts "Linking #{PADRINO_ROOT}/plugins/#{plugin}/templates/#{plugin} => #{PADRINO_ROOT}/templates/#{plugin}"
+        File.symlink "#{PADRINO_ROOT}/plugins/#{plugin}/templates/#{plugin}", "#{PADRINO_ROOT}/templates/#{plugin}"
+
+        if File.directory?("#{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin}") and not File.directory?("#{PADRINO_ROOT}/templates/mailer/#{plugin}")
+          puts "Linking #{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin} => #{PADRINO_ROOT}/templates/mailers/#{plugin}"
+          File.symlink "#{PADRINO_ROOT}/plugins/#{plugin}/mailers/#{plugin}", "#{PADRINO_ROOT}/templates/mailers/#{plugin}"
+        end
+      end
+    rescue Errno::EEXIST
+      # do nothing
+    end
+    Padrino.set_load_paths("#{PADRINO_ROOT}/plugins/#{plugin}", "#{PADRINO_ROOT}/plugins/#{plugin}/models")
+    require "#{PADRINO_ROOT}/plugins/#{plugin}/app"
+    Dir.glob("#{PADRINO_ROOT}/plugins/#{plugin}/models/*.rb").each{|r| require r.sub(/\.rb$/, '') }
+  end
+
+
+  
+  Cfg['apps'].each do |app, mountpoint|
+    begin
+      Object.const_get(app) # testing app existance
+
+      puts "Mounting App: #{app}..."
+      Padrino.mount("#{app}").to("#{mountpoint.downcase}").host(/^(?!(admin|www\.admin)).*$/)
+    rescue NameError
+      # do nothing
+      logger.error "not mounting #{app}"
+    end
+  end unless Cfg['apps'].nil?
+end
+###
