48845f0532ea2240e2664131f59e9abcdf4e04bd
Lorenzo Giuliani <lorenzo@frenzart.com>
2012-02-25 14:55:14 +0100

    ...

--- config/boot.rb
+++ config/boot.rb
@@ -8,7 +8,7 @@ require 'bundler/setup'
 Bundler.require(:default, PADRINO_ENV)
 
 MACHETE_PLUGINS = JSON.parse(File.read(Padrino.root('config', 'config.json')))['plugins'] << 'lesscss'
-MACHETE_PLUGINS_RX = new %r{(#{MACHETE_PLUGINS.join('|')})}
+MACHETE_PLUGINS_RX = %r{(#{MACHETE_PLUGINS.join('|')})}
 ##
 # Add here your before load hooks
 #
