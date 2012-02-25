ee7ac4151812db0387830e4ae36f89eb61e60318
Lorenzo Giuliani <lorenzo@frenzart.com>
2012-02-25 15:04:56 +0100

    Kernel.fork fucks up thin pid generation

--- plugins/lesscss/pre-boot.rb
+++ plugins/lesscss/pre-boot.rb
@@ -7,11 +7,9 @@ puts 'Compiling less files'
 
 path = ::File.expand_path('../public/stylesheets', __FILE__)
 
-Kernel.fork {
-  Dir.glob(::File.expand_path('../public/stylesheets/*.less', __FILE__)) do |f|
-    f = f.sub(path, '').sub(/^\//, '')
-    c = f.sub(/less$/, 'css')
-    puts "compiling #{path} / #{f}"
-    `cd #{path} && lessc #{f} > #{c}`
-  end
-}
+Dir.glob(::File.expand_path('../public/stylesheets/*.less', __FILE__)) do |f|
+  f = f.sub(path, '').sub(/^\//, '')
+  c = f.sub(/less$/, 'css')
+  puts "compiling #{path} / #{f}"
+  `cd #{path} && lessc #{f} > #{c}`
+end
