17725f962485bbbbb844e4138a1bf530de6f1fe6
Lorenzo Giuliani <lorenzo@frenzart.com>
2012-03-21 15:20:17 +0100

    added sticky subnav and a new block

--- public/javascripts/admin/application.js
+++ public/javascripts/admin/application.js
@@ -312,4 +312,27 @@
     });
   });
 
+  $.domReady(function(){
+    var $win = $(window)
+      , $nav = $('.subnav')
+      , navTop = $('.subnav').length && $('.subnav').offset().top
+      , isFixed = 0;
+
+    processScroll()
+  
+    $win.on('scroll', processScroll)
+  
+    function processScroll() {
+      var i, scrollTop = $win.scrollTop()
+      
+      if (scrollTop >= navTop && !isFixed) {
+        isFixed = 1
+        $nav.addClass('subnav-fixed')
+      } else if (scrollTop <= navTop && isFixed) {
+        isFixed = 0
+        $nav.removeClass('subnav-fixed')
+      }
+    }
+  });
+
 }(window.ender);
