d404e1283c8a0ed6e897683d5b3906b7b512fb08
Lorenzo Giuliani <lorenzo@frenzart.com>
2012-08-30 12:03:28 +0200

    faster less compiler

--- plugins/lesscss/pre-boot.rb
+++ plugins/lesscss/pre-boot.rb
@@ -5,11 +5,8 @@
 
 puts 'Compiling less files'
 
-path = ::File.expand_path('../public/stylesheets', __FILE__)
+path = ::File.expand_path('../../../public/stylesheets', __FILE__)
 
-Dir.glob(::File.expand_path('../public/stylesheets/*.less', __FILE__)) do |f|
-  f = f.sub(path, '').sub(/^\//, '')
-  c = f.sub(/less$/, 'css')
-  puts "compiling #{path} / #{f}"
-  `cd #{path} && lessc #{f} > #{c}`
+Thread.new do
+  `cd #{path} && ls *.less | awk '{print("lessc "$1" "$1)}' | sed 's/\\.less/\\.css/2' | /bin/bash`
 end
